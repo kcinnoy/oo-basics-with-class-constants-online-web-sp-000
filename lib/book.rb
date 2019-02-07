f331db554f9f4ed76708ff6509ec8a031fcfe8b0
Yonnick Brown <25hour24@gmail.com>
2019-02-07 22:19:05 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   attr_accessor :author, :page_count
   attr_reader :title, :genre
 
-  GENRE = []
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -12,7 +12,7 @@ class Book
 
   def genre=(genre)
     @genre = genre
-    GENRE << genre
+    GENRES << genre
   end
 
   def turn_page
